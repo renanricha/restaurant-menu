64d7ac389e42563fa1a99f3fa4c44d4fb6f08fa2
Renan Richa <renanricha@gmail.com>
2024-04-21 20:57:29 -0300

    Try to show data

--- cardapio-restaurante/src/service/index.js
+++ cardapio-restaurante/src/service/index.js
@@ -1,5 +1,20 @@
 import produtos from "../data/data-produtos.js"
+import estilos from "../styles/Home.module.css"
 
-export const retornarProdutos = () => {
-    return produtos
+const ListaProdutos = () => {
+    const produtosFiltrados = produtos.filter(
+        (produto) => produto.id > 0
+    )
+    return (
+        <div className={estilos.lista}>
+            {produtosFiltrados.map((produto) => (
+            <ProdutoExibir
+            key={produto.id}
+            nome={produto.nome}
+            />
+            ))}
+        </div>
+    )
 }
+
+export default ListaProdutos
